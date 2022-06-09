ea404c4cdb354b499d8615997dd711187736a17d
Barry Paneer <barrypaneer@BarrydeMacBook-Pro.local>
2022-06-09 16:40:47 +0800

    added

--- openassessment/xblock/static/js/src/lms/oa_file_upload.js
+++ openassessment/xblock/static/js/src/lms/oa_file_upload.js
@@ -16,13 +16,12 @@ Returns:
 */
 OpenAssessment.FileUploader = function() {
     this.upload = function(url, file) {
-        return $.Deferred(
-            function(defer) {
+        return new Promise(function (resolve, reject) {
                 $.ajax({
                     url: url,
                     type: 'PUT',
                     data: file,
-                    async: false,
+                    async: true,
                     processData: false,
                     contentType: file.type
                 }).done(
@@ -38,11 +37,11 @@ OpenAssessment.FileUploader = function() {
                         );
 
                         // Return control to the caller
-                        defer.resolve();
+                        resolve();
                     }
                 ).fail(
                     function(data, textStatus) {
-                        defer.rejectWith(this, [textStatus]);
+                        reject([textStatus]);
                     }
                 );
             }
